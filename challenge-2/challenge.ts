e8565f4d8a3bb33aaf9298530343bd48f4b3e621
bhushankhope <bhushankhope@gmail.com>
2024-09-21 15:24:07 -0400

    asdad

---
+++ challenge-2/challenge.ts
@@ -0,0 +1,11 @@
+/**
+ * The entry point function. This will read the provided CSV file, scrape the companies'
+ * YC pages, and output structured data in a JSON file.
+ */
+
+import Papa from 'papaparse';
+export async function processCompanyList() {
+  /**
+   * Put your code here!
+   */
+}
---
+++ challenge-2/runner.ts
@@ -0,0 +1,7 @@
+import { processCompanyList } from "./challenge";
+/**
+ * This is the entry point for the challenge.
+ * This will run your code.
+ */
+await processCompanyList();
+console.log("✅ Done!");
